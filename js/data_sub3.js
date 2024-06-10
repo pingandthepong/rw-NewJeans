7f3035d29586372ddbf62b1e12573afea6d264d0
pingandthepong <pingandthepong@gmail.com>
2024-06-10 10:17:15 +0900

    one page slide - menu click 이벤트 (함수화)

--- js/data_sub3.js
+++ js/data_sub3.js
@@ -1,185 +1,281 @@
 // 각 section에 .bg 생성 후 background를 img와 맞추기
-const sections = document.querySelectorAll("section");
-const imgs = document.querySelectorAll("section img");
+function setBG() {
+  const sections = document.querySelectorAll("section");
+  const imgs = document.querySelectorAll("section img");
 
-sections.forEach(function (section, secIdx) {
-  const bg = document.createElement("div");
-  bg.classList.add("bg");
-  section.prepend(bg);
+  sections.forEach(function (section, secIdx) {
+    const bg = document.createElement("div");
+    bg.classList.add("bg");
+    section.prepend(bg);
 
-  bg.style.background = `url(${imgs[secIdx].src}) no-repeat center`;
-  bg.style.backgroundSize = "cover";
-  bg.style.backgroundAttachment = "fixed";
+    bg.style.background = `url(${imgs[secIdx].src}) no-repeat center`;
+    bg.style.backgroundSize = "cover";
+    bg.style.backgroundAttachment = "fixed";
+  });
+}
+
+// ====================================
+// ========== one page slide ==========
+// ====================================
+
+// section 영역 높이를 브라우저 높이 값으로 자동 설정 (height: 100vh으로 해결했지만, script로도 가능함)
+function setHeight() {
+  const winH = window.innerHeight;
+  const sections = document.querySelectorAll("section");
+  sections.forEach(function (section) {
+    section.style.height = winH + "px";
+  });
+}
+
+// #menu 클릭시 자동 상하 스크롤 및 활성화 클래스 관리
+function setupMenu() {
+  const menuItems = document.querySelectorAll("#menu li");
+  const sections = document.querySelectorAll("section");
+
+  menuItems.forEach(function (menuItem, menuIdx) {
+    // #menu li animation delay
+    setTimeout(function () {
+      menuItem.classList.add("ani");
+    }, 200 * menuIdx);
+
+    // #menu 클릭시
+    menuItem.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const sectionIdx = Array.from(menuItems).indexOf(menuItem);
+      const targetSection = sections[sectionIdx];
+      targetSection.scrollIntoView({ behavior: "smooth" });
+
+      // #menu 클릭시 활성화
+      menuItems.forEach((item) => {
+        item.classList.remove("on");
+      });
+      menuItem.classList.add("on");
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setBG();
+  setHeight();
+  setupMenu();
 });
 
-// #menu li animation delay
-const menuList = document.querySelectorAll("#menu li");
-menuList.forEach(function (li, liIdx) {
-  setTimeout(function () {
-    li.classList.add("ani");
-  }, 200 * liIdx);
+document.addEventListener("resize", setHeight);
+
+// 화면 스크롤 시 현재 메뉴 활성화
+window.addEventListener("scroll", function () {
+  const scroll = window.scrollY || this.window.pageYOffset;
+
+  sections.forEach(function (section, i) {
+    if (scroll >= winH * i && scroll < winH * (i + 1)) {
+      menuItems.forEach(function (menuItem) {
+        menuItem.classList.remove("on");
+      });
+      // menuItem[i].classList.add("on");
+    }
+  });
 });
 
-// one page slide
+// $(document).ready(function () {
+//   // ==============================
+//   // section위에서 마우스 휠 이벤트 (delta는 휠의 방향을 감지 - 올리면 1, 내리면 -1)
+//   $("section").on("mousewheel", function (event, delta) {
+//     // 마우스 휠을 올렸을때
+//     if (delta > 0) {
+//       // 변수 prev에 현재 휠을 움직인 section에서 이전 section의 offset().top위치 저장
+//       var prev = $(this).prev().offset().top;
+//       // 문서 전체를 prev에 저장된 위치로 이동
+//       $("html,body").stop().animate({ scrollTop: prev }, 500);
+
+//       // wheel 이벤트가 여러 번 계산됨을 방지
+//       return false;
+
+//       // 마우스 휠을 내렸을때
+//     } else if (delta < 0) {
+//       // 변수 next에 현재 휠을 움직인 section에서 다음 section의 offset().top위치 저장
+//       var next = $(this).next().offset().top;
+//       // 문서 전체를 next에 저장된 위치로 이동
+//       $("html,body").stop().animate({ scrollTop: next }, 500);
+
+//       // wheel 이벤트가 여러 번 계산됨을 방지
+//       return false;
+//     }
+//   });
+// });
 
 // ==============================
-// music player
-const playBtn = document.querySelector(".playBtn");
-const backward = document.querySelector(".backward");
-const forward = document.querySelector(".forward");
-const audioEl = document.querySelector(".new_audio");
+// // music player
+// const playBtns = document.querySelectorAll(".playBtn");
+// const backwards = document.querySelectorAll(".backward");
+// const forwards = document.querySelectorAll(".forward");
+// const audioEls = document.getElementsByTagName(".new_audio");
 
-const tracks = [
-  "./files/attention.mp3",
-  "./files/hypeboy.mp3",
-  "./files/cookie.mp3",
-  "./files/hurt.mp3",
-];
+// const tracks = [
+//   "./files/attention.mp3",
+//   "./files/hypeboy.mp3",
+//   "./files/cookie.mp3",
+//   "./files/hurt.mp3",
+// ];
 
-let currentTrackIdx = 0;
-let status = false; // false(정지상태), true(재생중)
-let ctx, analyzer, animationId;
+// let currentTrackIdx = 0;
+// let status = false; // false(정지상태), true(재생중)
+// let ctx, analyzer, animationId;
 
-// 초기 상태 설정
-window.addEventListener("DOMContentLoaded", (event) => {
-  createVisualizerBars();
-  setPlayButton();
-  createVirtualEls();
-  loadTrack(currentTrackIdx);
-});
+// // 초기 상태 설정
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   createVisualizerBars();
+//   setPlayButton();
+//   loadTrack(currentTrackIdx);
+// });
 
-function setPlayButton() {
-  playBtn.addEventListener("click", function (e) {
-    e.preventDefault();
+// function setPlayButton() {
+//   playBtns.forEach(function (playBtn, index) {
+//     playBtn.addEventListener("click", function (e) {
+//       e.preventDefault();
 
-    if (status == false) {
-      // 정지상태일 때 클릭시
-      playAudio();
-      this.firstChild.classList.remove("fa-play");
-      this.firstChild.classList.add("fa-pause");
-      status = true;
-    } else {
-      // 재생중일 때 클릭시
-      pauseAudio();
-      this.firstChild.classList.remove("fa-pause");
-      this.firstChild.classList.add("fa-play");
-      status = false;
-    }
-  });
+//       // btn과 곡 제목 연결
+//       const songLists = document.querySelectorAll(".song_list li a");
+//       songLists.forEach(function (songList, idx) {
+//         if (idx === index) {
+//           songList.classList.add("on");
+//         } else {
+//           songList.classList.remove("on");
+//         }
+//       });
 
-  backward.addEventListener("click", function (e) {
-    e.preventDefault();
-    audioEl.currentTime = 0;
-    if (status == true) {
-      audioEl.play();
-    }
-  });
+//       if (status == false) {
+//         // 정지상태일 때 클릭시
+//         playAudio();
+//         this.firstChild.classList.remove("fa-play");
+//         this.firstChild.classList.add("fa-pause");
+//         status = true;
+//       } else {
+//         // 재생중일 때 클릭시
+//         pauseAudio();
+//         this.firstChild.classList.remove("fa-pause");
+//         this.firstChild.classList.add("fa-play");
+//         status = false;
+//       }
+//     });
+//   });
 
-  // TODO
-  forward.addEventListener("click", function (e) {
-    e.preventDefault();
-    nextTrack();
-  });
-}
+//   backwards.forEach(function (backward) {
+//     backward.addEventListener("click", function (e) {
+//       e.preventDefault();
 
-function createVisualizerBars() {
-  const visualizerContainer = document.querySelectorAll(
-    ".visualizer-container"
-  );
+//       audioEls.forEach(function (audioEl) {
+//         audioEl.currentTime = 0;
+//         if (status == true) {
+//           audioEl.play();
+//         }
+//       });
+//     });
+//   });
 
-  visualizerContainer.forEach(function (visualizer) {
-    visualizerContainer.innerHTML = ""; // 기존 바를 제거하고 새로 추가하도록 수정
+//   forwards.forEach(function (forward) {
+//     forward.addEventListener("click", function (e) {
+//       e.preventDefault();
+//       nextTrack();
+//     });
+//   });
+// }
 
-    for (let i = 0; i < 30; i++) {
-      // 30은 NBR_OF_BARS의 값
-      let bar = document.createElement("DIV");
-      bar.setAttribute("id", "bar" + i);
-      bar.setAttribute("class", "visualizer-container__bar");
-      visualizer.appendChild(bar);
-    }
-  });
-}
+// function createVisualizerBars() {
+//   const visualizerContainer = document.querySelectorAll(
+//     ".visualizer-container"
+//   );
 
-function createAudioContext() {
-  if (!ctx) {
-    ctx = new (window.AudioContext || window.webkitAudioContext)(); // 브라우저 호환성을 위해 수정
-    const audioSource = ctx.createMediaElementSource(audioEl);
-    analyzer = ctx.createAnalyser();
+//   visualizerContainer.forEach(function (visualizer) {
+//     visualizerContainer.innerHTML = ""; // 기존 바를 제거하고 새로 추가하도록 수정
 
-    audioSource.connect(analyzer);
-    audioSource.connect(ctx.destination);
-  }
-}
+//     for (let i = 0; i < 30; i++) {
+//       // 30은 NBR_OF_BARS의 값
+//       let bar = document.createElement("DIV");
+//       bar.setAttribute("id", "bar" + i);
+//       bar.setAttribute("class", "visualizer-container__bar");
+//       visualizer.appendChild(bar);
+//     }
+//   });
+// }
 
-function playAudio() {
-  if (!ctx) {
-    createAudioContext();
-  }
-  audioEl.volume = 0.3;
-  audioEl
-    .play()
-    .then(() => {
-      renderFrame();
-    })
-    .catch((error) => {
-      console.error("Error playing audio:", error);
-    });
-}
+// function createAudioContext(audioElement) {
+//   if (!ctx) {
+//     ctx = new (window.AudioContext || window.webkitAudioContext)(); // 브라우저 호환성을 위해 수정
+//     analyzer = ctx.createAnalyser();
 
-function pauseAudio() {
-  audioEl.pause();
-  cancelAnimationFrame(animationId); // 중지 상태에서는 애니메이션도 중지
-}
+//     const audioSource = ctx.createMediaElementSource(audioElement);
+//     audioSource.connect(analyzer);
+//     audioSource.connect(ctx.destination);
+//   }
+// }
 
-function renderFrame() {
-  const frequencyData = new Uint8Array(analyzer.frequencyBinCount);
-  analyzer.getByteFrequencyData(frequencyData);
+// function playAudio() {
+//   const audioElement = audioEls[currentTrackIdx];
+//   createAudioContext(audioElement);
+//   audioElement.volume = 0.3;
+//   audioElement
+//     .play()
+//     .then(() => {
+//       renderFrame();
+//     })
+//     .catch((error) => {
+//       console.error("Error playing audio:", error);
+//     });
+// }
 
-  for (let i = 0; i < 30; i++) {
-    // 30은 NBR_OF_BARS의 값
-    const index = (i + 15) * 2;
-    const fd = frequencyData[index];
-    const bar = document.querySelector("#bar" + i);
-    if (bar) {
-      const barHeight = Math.max(4, fd || 0);
-      bar.style.height = barHeight + "px";
-    }
-  }
+// function pauseAudio() {
+//   audioEls[currentTrackIdx].pause();
+//   cancelAnimationFrame(animationId); // 중지 상태에서는 애니메이션도 중지
+// }
 
-  animationId = window.requestAnimationFrame(renderFrame);
-}
+// function renderFrame() {
+//   const frequencyData = new Uint8Array(analyzer.frequencyBinCount);
+//   analyzer.getByteFrequencyData(frequencyData);
 
-function loadTrack(index) {
-  audioEl.src = tracks[index];
-  audioEl.load();
-  if (status === true) {
-    playAudio();
-  }
-}
+//   for (let i = 0; i < 30; i++) {
+//     // 30은 NBR_OF_BARS의 값
+//     const index = (i + 15) * 2;
+//     const fd = frequencyData[index];
+//     const bar = document.querySelector("#bar" + i);
+//     if (bar) {
+//       const barHeight = Math.min(Math.max(4, fd || 0), 150); // minHeight4, maxHeight100으로 설정
+//       bar.style.height = barHeight + "px";
+//     }
+//   }
 
-function nextTrack() {
-  currentTrackIdx = (currentTrackIdx + 1) % tracks.length;
-  loadTrack(currentTrackIdx);
-}
+//   animationId = window.requestAnimationFrame(renderFrame);
+// }
 
-// .badge_title
-const badgeTitles = document.querySelectorAll(".badge_title a");
+// function loadTrack(index) {
+//   audioEls[index].src = tracks[index];
+//   audioEls[index].load();
+//   // if (status === true) {
+//   //   playAudio(audioEl);
+//   // }
+// }
 
-badgeTitles.forEach(function (badgeTitle) {
-  const spanTitle = document.createElement("span");
-  spanTitle.classList.add("spanTitle");
-  badgeTitle.appendChild(spanTitle);
-  spanTitle.textContent = "TITLE";
-});
+// function nextTrack() {
+//   currentTrackIdx = (currentTrackIdx + 1) % tracks.length;
+//   loadTrack(currentTrackIdx);
+// }
 
-// .badge_hit
-const badgeHits = document.querySelectorAll(".badge_hit a");
+// // .badge_title
+// const badgeTitles = document.querySelectorAll(".badge_title a");
 
-badgeHits.forEach(function (badgeHit) {
-  const spanHit = document.createElement("span");
-  spanHit.classList.add("spanHit");
-  badgeHit.appendChild(spanHit);
-  spanHit.textContent = "HIT";
-});
+// badgeTitles.forEach(function (badgeTitle) {
+//   const spanTitle = document.createElement("span");
+//   spanTitle.classList.add("spanTitle");
+//   badgeTitle.appendChild(spanTitle);
+//   spanTitle.textContent = "TITLE";
+// });
 
-// 음악 play될 때, 제목 on
+// // .badge_hit
+// const badgeHits = document.querySelectorAll(".badge_hit a");
+
+// badgeHits.forEach(function (badgeHit) {
+//   const spanHit = document.createElement("span");
+//   spanHit.classList.add("spanHit");
+//   badgeHit.appendChild(spanHit);
+//   spanHit.textContent = "HIT";
+// });
