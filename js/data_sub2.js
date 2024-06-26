f688ca3ff40dcddfba643a52b6ce0faf83c3deaf
pingandthepong <pingandthepong@gmail.com>
2024-06-27 07:59:45 +0900

    sub2 각 곡 클릭할 때 play & pause 기능 추가(playBtn, pauseBtn 클릭할 때만 적용되도록 수정)

--- js/data_sub2.js
+++ js/data_sub2.js
@@ -58,6 +58,7 @@ window.addEventListener("resize", () => {
 
 // DONE: playlist 기능
 const audio = document.querySelector(".new_audio");
+const fullPlay = document.querySelector("#fullPlay");
 let currentAudioIdx = 0; // 현재 재생 중인 오디오 인덱스
 
 // DONE: 곡마다 재생
@@ -65,20 +66,65 @@ titles.forEach((title, titleIdx) => {
   title.addEventListener("click", function (e) {
     e.preventDefault();
 
-    // 같은 제목을 다시 클릭한 경우
+    // 같은 제목을 다시 클릭한 경우 (음악이 재생중일 때)
     if (titleIdx === currentAudioIdx && !audio.paused) {
+      console.log("이벤트 발생1");
       audio.pause();
-      titles[titleIdx].classList.remove("pauseBtn");
-      titles[titleIdx].classList.add("playBtn");
+
+      // 전체 title의 pauseBtn 제거
+      titles.forEach((t) => {
+        t.classList.remove("pauseBtn");
+      });
+
+      console.log(titles[titleIdx]);
     } else {
+      console.log("이벤트 발생2");
+
       // 제목 클릭한 경우 (같은제목X)
       audio.src = `./files/new${titleIdx + 1}.mp3`;
       audio.play();
+
+      // 전체 title의 pauseBtn, playBtn 제거
+      titles.forEach((t) => {
+        t.classList.remove("pauseBtn");
+        t.classList.remove("playBtn");
+      });
+      // 자기 자신만 pauseBtn
       titles[titleIdx].classList.add("pauseBtn");
+
+      console.log(title);
     }
 
     currentAudioIdx = titleIdx;
   });
 });
 
-// TODO: 전체 재생
+// // TODO: 전체 재생
+// fullPlay.addEventListener("click", function () {
+//   // 첫 곡 재생을 위해 초기화
+//   currentAudioIdx = 0;
+
+//   // 재생이 끝나면 다음 곡 재생
+//   function playNext() {
+//     // 현재 재생 중인 곡인 경우
+//     if (!audio.paused) {
+//       // 다음 곡의 인덱스를 증가시킵니다.
+//       currentAudioIdx++;
+//       // 모든 곡이 재생된 경우 처음 곡으로 되돌아갑니다.
+//       if (currentAudioIdx >= titles.length) {
+//         currentAudioIdx = 0;
+//       }
+//       audio.src = `./files/new${currentAudioIdx + 1}.mp3`;
+//       audio.play();
+//       // 현재 타이틀에 'pauseBtn' 클래스를 추가하고 이전 타이틀의 클래스를 제거합니다.
+//       titles.forEach((title) => title.classList.remove("pauseBtn"));
+//       titles[currentAudioIdx].classList.add("pauseBtn");
+//     }
+//   }
+
+//   // 첫 곡 재생
+//   playNext();
+
+//   // 재생이 끝날 때마다 다음 곡을 재생합니다.
+//   audio.addEventListener("ended", playNext);
+// });
